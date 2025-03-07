d51e7e9cc8ac9e47b7408b0a370dfd942203a8d0
Nikoleta79 <nikoletatsitsoli@gmail.com>
2025-03-07 22:08:35 +0100

    Initial commit: Vue.js bike tracking app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import L from "leaflet";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const map = L.map("map").setView([52.3676, 4.9041], 13); // Default: Amsterdam
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap contributors",
+  }).addTo(map);
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const userLocation = [position.coords.latitude, position.coords.longitude];
+      L.marker(userLocation).addTo(map).bindPopup("You are here!").openPopup();
+      map.setView(userLocation, 14);
+    });
+  }
+});
 </script>
 
 <template>
@@ -11,7 +28,10 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+
   <HelloWorld msg="Vite + Vue" />
+
+  <div id="map"></div>
 </template>
 
 <style scoped>
@@ -27,4 +47,10 @@ import HelloWorld from './components/HelloWorld.vue'
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+#map {
+  width: 100%;
+  height: 500px;
+}
 </style>
+
+
